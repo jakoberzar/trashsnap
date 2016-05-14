4edf5227837787db832f8b4b26ba43303981dee9
Jakob Erzar <jakob.erzar@gmail.com>
2016-05-14 17:59:38 +0200

    Dodana basic funkcionalnost za dodajanje Entryjev v kodi in (mogoče delujoče) postanje na API

---
+++ TrashSnap/API.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace TrashSnap
+{
+    public static class API
+    {
+        private static string _baseUrl = "http://greenplease.com" + "/api/v1/";
+        static string BaseUrl { get {
+                return _baseUrl;
+            }
+        }
+
+        /// <summary>
+        /// Prenese entry na server, return = uspelo ali ne
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <returns>Uspeh</returns>
+        static bool UploadEntry(Entry entry)
+        {
+            try
+            {
+                var imageStream = new ByteArrayContent(entry.Photo);
+                imageStream.Headers.ContentDisposition = new ContentDispositionHeaderValue("photo")
+                {
+                    FileName = Guid.NewGuid() + ".jpg"
+                };
+                var contentOtherParameters = new FormUrlEncodedContent(entry.Values());
+
+                var multi = new MultipartContent();
+                multi.Add(imageStream);
+                multi.Add(contentOtherParameters); // I guess it should work?
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(BaseUrl);
+                var result = client.PostAsync("upload", multi).Result;
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TrashSnap/Entry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrashSnap
+{
+    public class Entry
+    {
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+        public string Text { get; set; }
+        public byte[] Photo { get; set; }
+
+        /// <summary>
+        /// Vrne čas vnosa kot string, v formatu YYYY-MM-DD HH:MM:SS. (za API)
+        /// Le getter, dokler le uploadamo, potem pa bo tudi setter.
+        /// </summary>
+        public string Time {
+            get {
+                return TimeDate.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+        }
+
+        /// <summary>
+        /// Čas vnosa kot DateTime. Get (za app, ne api) in set.
+        /// </summary>
+        public DateTime TimeDate {  get; set; }
+        
+        public Entry() { }
+        public Entry(string text, float longitude, float latitude, byte[] photo) : this(text, longitude, latitude, photo, DateTime.Now) { }
+        public Entry(string text, float longitude, float latitude, byte[] photo, DateTime time)
+        {
+            this.Text = text;
+            this.Longitude = longitude;
+            this.Latitude = latitude;
+            this.Photo = photo;
+            this.TimeDate = time;
+        }
+
+        public Dictionary<string, string> Values()
+        {
+            Dictionary<string, string> d = new Dictionary<string, string>();
+            d.Add("longitude", Longitude.ToString());
+            d.Add("latitude", Latitude.ToString());
+            d.Add("text", Text);
+            return d;
+        }
+    }
+}
