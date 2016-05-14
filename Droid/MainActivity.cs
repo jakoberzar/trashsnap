9694f0cbb08bf12f51d974a9882c49aa34baadef
domen <domen.jeric@dspot.si>
2016-05-15 00:57:01 +0200

    Dodana kamera, funkcionalnost

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,30 +1,129 @@
-﻿using Android.App;
-using Android.Widget;
+﻿using System;
+
+using Android.App;
 using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Android.Hardware;
+using Android.Graphics;
+using Android.Hardware.Camera2;
+using Android.Hardware.Camera2.Params;
+
+
 
 namespace TrashSnap.Droid
 {
-	[Activity (Label = "TrashSnap", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+
+
+	[Activity (Label = "TrashSnap", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/MyTheme")]
+	public class MainActivity : Activity, TextureView.ISurfaceTextureListener
 	{
-		int count = 1;
+		Android.Hardware.Camera _camera;
+		TextureView _textureView;
+		Button _takePicture;
 
-		protected override void OnCreate (Bundle savedInstanceState)
+		protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (savedInstanceState);
-
-			// Set our view from the "main" layout resource
+			base.OnCreate (bundle);
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
+			_textureView = FindViewById<TextureView> (Resource.Id.textureView1);
+			_textureView.SurfaceTextureListener = this;
+
+			_takePicture = FindViewById<Button> (Resource.Id.but2);
+
+			_takePicture.Click += CaptureImage;
+
+
+			_textureView.SurfaceTextureListener = this;
+
+		}
+
+		public void OnSurfaceTextureAvailable (Android.Graphics.SurfaceTexture surface, int w, int h)
+		{
+			_camera = Android.Hardware.Camera.Open ();
+
+			var surfaceOrientation = WindowManager.DefaultDisplay.Rotation;
+			Matrix transform = new Matrix();
+
+			if (surfaceOrientation == SurfaceOrientation.Rotation0 || surfaceOrientation == SurfaceOrientation.Rotation180) {
+				_camera.SetDisplayOrientation (90);
+				_textureView.LayoutParameters = new FrameLayout.LayoutParams (w, h, GravityFlags.Center);
+				transform.SetScale (-1, 1);
+			} else {
+				_textureView.LayoutParameters = new FrameLayout.LayoutParams (w, h, GravityFlags.Center);
+			}
+
+			try {
+				_camera.SetPreviewTexture (surface);
+				_camera.StartPreview ();
+				//_textureView.SetTransform(transform);
+			} catch (Java.IO.IOException ex) {
+				Console.WriteLine (ex.Message);
+			}
+		}
+
+		public bool OnSurfaceTextureDestroyed (Android.Graphics.SurfaceTexture surface)
+		{
+			_camera.StopPreview ();
+			_camera.Release ();
+
+			return true;
+		}
+
+		public void OnSurfaceTextureSizeChanged (Android.Graphics.SurfaceTexture surface, int width, int height)
+		{
+			// camera takes care of this
+		}
+
+		public void OnSurfaceTextureUpdated (Android.Graphics.SurfaceTexture surface)
+		{
+
+		}
+
+
+		public void OnPictureTaken(byte[] data, Android.Hardware.Camera camera)
+		{
+			camera.StopPreview();
+			Toast.MakeText(this, "Cheese", ToastLength.Short).Show();
+			camera.StartPreview();
+		}
+
+
+		public void CaptureImage(object sender, EventArgs e) {
+			Bitmap bm = _textureView.Bitmap;
+		}
+
+
+	}
+
+
+	public class CameraSurfaceTextureListener : Java.Lang.Object, TextureView.ISurfaceTextureListener
+	{
+		private MainActivity Activity;
+		public CameraSurfaceTextureListener(MainActivity oActivity)
+		{
+			Activity = oActivity;
+		}
+		public void OnSurfaceTextureAvailable (Android.Graphics.SurfaceTexture oSurface, int iWidth, int iHeight)
+		{
 			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+		}
+		public bool OnSurfaceTextureDestroyed (Android.Graphics.SurfaceTexture oSurface)
+		{
+			return true;
+		}
+		public void OnSurfaceTextureSizeChanged (Android.Graphics.SurfaceTexture oSurface, int iWidth, int iHeight)
+		{
+			
+		}
+		public void OnSurfaceTextureUpdated (Android.Graphics.SurfaceTexture oSurface)
+		{
 		}
 	}
+
+		
+
 }
 
 
--- TrashSnap/MyClass.cs
+++ TrashSnap/MyClass.cs
@@ -6,6 +6,7 @@ namespace TrashSnap
 	{
 		public MyClass ()
 		{
+			
 		}
 	}
 }
